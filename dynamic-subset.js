b95cea97462d6229ea7b34e50e803de6b3dcd60e
alstjr7375 <alstjr7375@daum.net>
2021-10-18 17:51:19 +0900

    Dynamic Subset #26

---
+++ dynamic-subset.js
@@ -0,0 +1,60 @@
+// == Load Library =============================================================
+const join      = require("path").join;
+const readdir   = require('fs').readdir;
+const unlink    = require('fs').unlink;
+const fontRange = require("font-range").fontRange;
+
+// == Constants ================================================================
+const INPUT_PATH = join("dist", "public", "static");
+const OUTPUT_PATH = join("dist", "web", "static");
+const FONTLISTS = [
+  "Pretendard-Black",
+  "Pretendard-Bold",
+  "Pretendard-ExtraBold",
+  "Pretendard-ExtraLight",
+  "Pretendard-Light",
+  "Pretendard-Medium",
+  "Pretendard-Regular",
+  "Pretendard-SemiBold",
+  "Pretendard-Thin"
+];
+
+const errCallback = (err) => {
+  if(err) {
+    console.error(err);
+    return true;
+  }
+  return false;
+};
+
+// == Dynamic Subset ===========================================================
+function dynamic_subset(format) {
+  const outputDir = join(OUTPUT_PATH, format + "-dynamic-subset");
+  const nameFormat = "{NAME}.subset.{INDEX}{EXT}";
+
+  // Remove Files
+  readdir(outputDir, (err, fileLists) => {
+    if(errCallback(err)) {
+      return;
+    }
+    for( const fontFile of fileLists ) {
+      const fontPath = join(outputDir, fontFile);
+      unlink(fontPath, errCallback);
+    }
+  });
+
+  // Create Files
+  for ( const fontName of FONTLISTS ) {
+    const cssFile = join(OUTPUT_PATH, fontName + ".css");
+    const fontFile = join(INPUT_PATH, fontName + ".otf");
+    fontRange(cssFile, fontFile, {
+      savePath: outputDir,
+      format: format,
+      nameFormat: nameFormat
+    });
+  }
+}
+
+// == Subset Apply  ============================================================
+dynamic_subset("woff");
+dynamic_subset("woff2");

a4bdd66a93b9381f17647b7f62e4db8ce71bd81e
MS_Y <alstjr7375@daum.net>
2022-06-20 09:08:34 +0900

    fix: `dist/` css files for jsdelivr

---
+++ distCSS.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ROOT_DIR="$PWD"
+PACKAGE_LIST=("pretendard" "pretendard-jp" "pretendard-std")
+KIND_LIST=("static" "variable")
+
+rm -rf ./dist
+for packageDir in "${PACKAGE_LIST[@]}"; do
+  srcDir="packages/${packageDir}/dist"
+
+  for kind in "${KIND_LIST[@]}"; do
+    kindDir="${srcDir}/web/${kind}/"
+    distDir="${ROOT_DIR}/dist/web/${kind}/"
+    faceURL_SRC="\./"
+    faceURL_DIST="../${kindDir}"
+
+    cd "${ROOT_DIR}/${kindDir}"
+    sed -i "s/${faceURL_SRC//\//\\\/}/${faceURL_DIST//\//\\\/}/g" *.css
+    mkdir -p "${distDir}" && cp *.css "${distDir}"
+  done
+done
+
+cd "${ROOT_DIR}"
+git checkout HEAD -- packages/
